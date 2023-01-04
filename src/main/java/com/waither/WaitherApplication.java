65386c06c816aeedb0a04f2e3b0642bcddc57a61
DDonghyeo <gsu9045000@gmail.com>
2023-01-05 02:46:11 +0900

    Initial commit

---
+++ src/main/java/com/waither/WaitherApplication.java
@@ -0,0 +1,13 @@
+package com.waither;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waither/WaitherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waither;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
