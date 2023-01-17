6e212e9ef6c5ffc88981775aaa4002a31a5f0a70
kimehhk <kimehhk@gmail.com>
2023-01-17 10:41:51 +0900

    [FEAT] WebConfig

---
+++ src/main/java/com/waither/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.waither.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600; //1H
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*") // 외부에서 들어오는 모든 url을 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용되는 Method
+                .allowedHeaders("*") // 허용되는 헤더
+                .allowCredentials(true) // 자격증명 허용
+                .maxAge(MAX_AGE_SECS); // 허용 시간
+    }
+}
