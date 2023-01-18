f32c48525cb7ff7a1c23f966ff32ba5b23f8abad
shhb1022 <shhb1022@naver.com>
2023-01-18 17:47:01 +0900

    [Feat] get UltraSrtNcst

--- src/main/java/com/waither/WaitherApplication.java
+++ src/main/java/com/waither/WaitherApplication.java
@@ -3,7 +3,6 @@ package com.waither;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
