5b548d2145b985083111a9d8310bb10456460124
DDonghyeo <gsu9045000@gmail.com>
2023-02-01 19:22:28 +0900

    [Fix] Merge Conflict resolved

--- src/main/java/com/waither/WaitherApplication.java
+++ src/main/java/com/waither/WaitherApplication.java
@@ -3,10 +3,12 @@ package com.waither;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import java.io.IOException;
 
-//Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.를 위해 db사용X 추가
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableWebMvc
+@SpringBootApplication
 public class WaitherApplication {
 
 	public static void main(String[] args) {
